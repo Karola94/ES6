15c0fa2019c5d10ef88678def12f15be42afe60e
Karolina Kuchta <linka_94@interia.eu>
2019-09-17 13:25:19 +0200

    5 tasks done

--- script.js
+++ script.js
@@ -9,5 +9,18 @@ const multiply = (a = 1,b = 1) => console.log(a*b);
 multiply(2);
 
 //Zadanie 3
-const average = (...args) => [...args].reduce((a, b) => (a + b)/(), 0);
-const logAllArguments = (...args) => args.forEach(arg => console.log(arg));
+const logAllArguments = (...args) => {
+    let score = 0;
+    args.map(arg => score+=arg);
+    console.log(parseFloat(score/args.length, 4).toFixed(2));
+  }  
+logAllArguments(1,2,3,8);
+
+//Zadanie 4
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+logAllArguments(...grades);
+
+//Zadanie 5
+const tab = [1, 4, 'Iwona', false, 'Nowak'];
+const [first, second, firstName, fourth, lastName] = tab;
+console.log(`${firstName} ${lastName}`);
